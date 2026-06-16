fa995fa1a3bece82257f4f9ff7e3696190701f96
Natka <n.charzynska.476@studms.ug.edu.pl>
2026-06-16 19:21:48 +0200

    Added Keycloak

--- app/api/axios.js
+++ app/api/axios.js
@@ -3,7 +3,17 @@ import toast from "react-hot-toast";
 
 const api = axios.create({
     baseURL: "/api",
-    withCredentials: true,
+    // withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
 });
 
 api.interceptors.response.use(
